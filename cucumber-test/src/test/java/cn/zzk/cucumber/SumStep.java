a09f3106b8a3e3a5e5d411027a0967b455987be1
zzkskys <2336276765@qq.com>
2021-05-26 14:56:49 +0800

    测试 cucumber

--- cucumber-test/src/test/java/cn/zzk/cucumber/SumStep.java
+++ cucumber-test/src/test/java/cn/zzk/cucumber/SumStep.java
@@ -14,21 +14,21 @@ public class SumStep {
 
     private int c;
 
-//    @假如("我们有两个整数{int}和{int}")
-//    public void 我们有两个整数A和B(int int1, int int2) {
-//        a = int1;
-//        b = int2;
-//
-//    }
-//
-//    @那么("我们将得到数值{int}")
-//    public void 我们将得到数值C(int temp) {
-//        assertEquals(c, temp);
-//
-//    }
-//
-//    @当("将其求和")
-//    public void 将其求和() {
-//        c = a + b;
-//    }
+    @假如("我们有两个整数{int}和{int}")
+    public void 我们有两个整数A和B(int int1, int int2) {
+        a = int1;
+        b = int2;
+
+    }
+
+    @那么("我们将得到数值{int}")
+    public void 我们将得到数值C(int temp) {
+        assertEquals(c, temp);
+
+    }
+
+    @当("将其求和")
+    public void 将其求和() {
+        c = a + b;
+    }
 }
